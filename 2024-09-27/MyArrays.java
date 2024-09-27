db8181b73683596effe2f09ee4d9d6e9cc5d30cb
andrewg454 <andrewg454@nycstudents.net>
2024-09-27 13:58:50 +0000

    test case for returnCopy

--- 2024-09-27/MyArrays.java
+++ 2024-09-27/MyArrays.java
@@ -8,7 +8,12 @@ public class MyArrays{
         endString += "]";
         return endstring.substring(0, endstring.length() -2);
     }
+public static int[] returnCopy(int[]ary){
+    int[] newArray = new int[ary.length()];
+}
     public static void main(String[] args){
-
+        int[] testArray = {1,2,3,4,5,6,10};
+        System.out.println("result should be" + arrayToString(testArray) + ", actual result is " + arrayToString(returnCopy(testArray)))
+        
     }
 } 
