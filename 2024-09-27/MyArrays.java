73ac50dc9af986089f3380414be2b67e5cc1fd16
andrewg454 <andrewg454@nycstudents.net>
2024-09-27 14:14:04 +0000

    concatArray code v1

--- 2024-09-27/MyArrays.java
+++ 2024-09-27/MyArrays.java
@@ -13,8 +13,25 @@ public static int[] returnCopy(int[]ary){
     }
     return newArray;
 }
+public static int[] concatArray(int[]ary1,int[]ary2){
+        int[] result = new int[nums.length];
+    int index = 0;
+    for (int i = 0; i < nums.length; i++) {
+            result[index] = ary1[i];
+            index++;
+    }
+    for (int i = 0; i < nums.length; i++) {
+            result[index] = ary2[i];
+            index++;
+    }
+
+    return result;
+}
+
+}
     public static void main(String[] args){
         int[] testArray = {1,2,3,4,5,6,10};
+        int[] testArray2 = {2, 4, 6, 8, 100};
         System.out.println("result should be" + arrayToString(testArray) + ", actual result is " + arrayToString(returnCopy(testArray)));
         System.out.println("They aren't the same because the test array's address is " +  testArray + " and the address of the function array is " + returnCopy(testArray));
         
