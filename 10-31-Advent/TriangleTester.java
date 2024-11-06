00ef997df6741eabb4a3196ef3623f1553d37cd2
andrewg454 <andrewg454@nycstudents.net>
2024-11-06 01:09:14 +0000

    countTrianglesB

--- 10-31-Advent/TriangleTester.java
+++ 10-31-Advent/TriangleTester.java
@@ -6,7 +6,7 @@ public class TriangleTester {
 
   public static void main(String[] args){
     System.out.println(countTrianglesA("inputTri.txt"));
-
+    System.out.println(countTrianglesB("inputTri.txt"));
   }
 
   public static int countTrianglesA(String filename){
@@ -39,26 +39,35 @@ public class TriangleTester {
 
     //return (Math.max(Integer.parseInt(array[0]), Integer.parseInt(array[1]), Integer.parseInt(array[2])) < (Integer.parseInt(array[0]) + Integer.parseInt(array[1]) + Integer.parseInt(array[2]) - Math.max(Integer.parseInt(array[0]), Integer.parseInt(array[1]), Integer.parseInt(array[2]))));
   }
+  public static boolean checkTriangleB(int a, int b, int c){
+    return (a + b >c && c + b > a && a + c > b);}
   public static int countTrianglesB(String filename){
     int count = 0;
     Scanner input = new Scanner(filename);
     int rowCount = 0;
-            while (scanner.hasNextLine()) {
-                scanner.nextLine();
+            while (input.hasNextLine()) {
+                input.nextLine();
                 rowCount++;
             }
     input = new Scanner(filename);
     int[][] numbers = new int[rowCount][3];
-    int i = 0;
-    while (scanner.hasNext()) {
-        numbers[rowIndex][0] = scanner.nextInt();
-        numbers[rowIndex][1] = scanner.nextInt();
-        numbers[rowIndex][2] = scanner.nextInt();
-        rowIndex++;
+    int ind = 0;
+    while (input.hasNext()) {
+        numbers[ind][0] = input.nextInt();
+        numbers[ind][1] = input.nextInt();
+        numbers[ind][2] = input.nextInt();
+        ind++;
         }
-        
-  }
+    int numRows = rowCount / 3;
+    for (int i = 0; i < numRows; i++) {
+        int a = numbers[i][0];
+        int b = numbers[i + numRows][1];
+        int c = numbers[i + 2 * numRows][2];
+    if (checkTriangleB(a, b, c)){
+      count +=1;
+    }}
 
-}
+return count;
+}}
 
 //95
