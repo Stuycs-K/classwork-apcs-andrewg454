7e047c670e50cbb801035a142f78c01a9dcaecda
Andrew <andrewg454@nycstudents.net>
2024-11-19 10:14:37 -0500

    end of class

--- 2024-11-18-advent2016/Driver.java
+++ 2024-11-18-advent2016/Driver.java
@@ -5,10 +5,21 @@ import java.util.Arrays;
 import java.util.ArrayList;
 public class Driver{
   public static void main(String[] args){
-    String[] str = {"L1", "L2", "R3"};
+    String[] str = {"L2", "L3"};
       System.out.println(day1(str));
+      System.out.println(data());
     }
-//  public static String[] data()
+  public static String[]data(){
+    try{
+      Scanner inf = new Scanner(new File("text.txt"));
+      String line = inf.nextLine();
+      System.out.println(line);
+    }
+    catch(Exception e){
+      System.exit(1);
+    }
+    return null;
+  }
 public static int day1(String[] arr){
     int count = 0;
     int forward = 0;
@@ -18,30 +29,34 @@ public static int day1(String[] arr){
     int direction = 1;
     for (int i = 0; i < arr.length; i++){
       if(arr[i].contains("L")){
-        if (direction == 0){
+        if (direction == 1){
           direction = 4;
           left += Integer.parseInt(arr[i].substring(1));
-      //    System.out.println("im adding" + Integer.parseInt(arr[i].substring(1)) + "to the left");
+          System.out.println("im adding" + Integer.parseInt(arr[i].substring(1)) + "to the left");
         }
-        if(direction != 0){
+        else{
           direction -= 1;
           if (direction == 1){
             forward += Integer.parseInt(arr[i].substring(1));
+            System.out.println("im adding" + Integer.parseInt(arr[i].substring(1)) + "to the left");
           }
           if (direction == 2){
             right += Integer.parseInt(arr[i].substring(1));
+
+            System.out.println("im adding" + Integer.parseInt(arr[i].substring(1)) + "to the left");
           }
           if (direction == 3){
-            backward += Integer.parseInt(arr[i].substring(1));}
-          }
-        }
+            backward += Integer.parseInt(arr[i].substring(1));
+          System.out.println("im adding" + Integer.parseInt(arr[i].substring(1)) + "to the left");}
+
+        }}
         if(arr[i].contains("R")){
           if (direction == 4){
             direction = 1;
             forward += Integer.parseInt(arr[i].substring(1));
           }
           if(direction != 4){
-            direction +=1;
+            direction +=1;}
             if (direction == 2){
               right += Integer.parseInt(arr[i].substring(1));
             }
@@ -57,7 +72,7 @@ public static int day1(String[] arr){
 
 
 //System.out.println(left + right + forward + backward);
-  }
+
 
   count += Math.abs(left-right);
   count += Math.abs(forward-backward);
