128c457bb732e552a5c464c4214090f2654153c3
Andrew <andrewg454@nycstudents.net>
2024-11-20 10:14:35 -0500

    end of class, finished day2 func

---
+++ 2024-11-18-advent2016/Driver2.java
@@ -0,0 +1,36 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+import java.util.Arrays;
+import java.util.ArrayList;
+public class Driver2{
+  public static void main(String[] args){
+    System.out.println(Day2("UDLRUDLR"));
+
+  }
+  public static String Day2(String str){
+    //String code = "";
+    String[][] keyPad =
+    {{"1", "2", "3"}
+    ,{"4", "5", "6"}
+    ,{"7", "8", "9"}
+    };
+    int row = 1;
+    int column = 1;
+    for (int i = 0; i< str.length(); i++){
+      if (str.charAt(i) == 'U' && row > 0){
+        row--;
+      }
+      if (str.charAt(i) == 'D' && row < 2){
+        row++;
+      }
+      if (str.charAt(i) == 'L' && column > 0){
+        column--;
+      }
+      if (str.charAt(i) == 'R' && column < 2){
+        column++;
+      }
+    }
+    return (keyPad[row][column]);
+  }
+}
