2eaffc2777d6d9db05f521c37ea91525c6805d30
Andrew Glick <andrewg454@nycstudents.net>
2024-11-24 16:43:50 +0000

    done!

--- 2024-11-18-advent2016/Driver7.java
+++ 2024-11-18-advent2016/Driver7.java
@@ -5,8 +5,10 @@ import java.util.Arrays;
 import java.util.ArrayList;
 public class Driver7{
     public static void main(String[] args){
-     //   System.out.println((readFile("text7.txt")).toString());
+      //  System.out.println((readFile("text7.txt")).toString());
         System.out.println(Day7(readFile("text7.txt")));
+     //String[] test = {"luqpeubugunvgzdqk[jfnihalscclrffkxqz]wvzpvmpfiehevybbgpg[esjuempbtmfmwwmqa]rhflhjrqjbbsadjnyc"};
+     //   System.out.println(Day7(test));
     }
     public static String[] readFile(String fileName) {
     try (Scanner inf = new Scanner(new File(fileName));) {
@@ -28,21 +30,36 @@ public class Driver7{
     int count = 0;
     for (int i = 0; i < arr.length; i++) {
         String str = arr[i];
-        for (int z = 0; z < str.length() - 4; z++) { 
-            if ((str.substring(z, z + 1).equals("[")) &&
-                (str.substring(z + 5, z + 6).equals("]")) &&
-                (str.charAt(z + 1) == str.charAt(z + 4)) &&
-                (str.charAt(z + 2) == str.charAt(z + 3)) &&
-                (str.charAt(z + 1) != str.charAt(z + 2))) {
+        for (int z = 0; z < str.length() - 6; z++) { 
+            if (str.substring(z, z + 1).equals("[")){
+                int indx = str.substring(z).indexOf("]");
+                System.out.println(indx +z);
+                for(int k = z; k < indx + z; k++){
+                    if((str.charAt(k + 1) == str.charAt(k + 4)) &&
+                (str.charAt(k + 2) == str.charAt(k + 3)) &&
+                (str.charAt(k + 1) != str.charAt(k + 2))){
                 valid = false;
+                System.out.println("change");
+                }
+                }
             }
+            //&&
+                //(str.substring(z + 5, z + 6).equals("]")) &&
+              //  (str.charAt(z + 1) == str.charAt(z + 4)) &&
+              //  (str.charAt(z + 2) == str.charAt(z + 3)) &&
+               // (str.charAt(z + 1) != str.charAt(z + 2))) {
+              //  valid = false;
+               // System.out.println("change");
+          //  }
+           // System.out.println(str.substring(z, z + 1));
+          //  System.out.println(str.substring(z + 5, z + 6));
         }
         for (int z = 0; z < str.length() - 3; z++) { 
             if ((str.charAt(z) == str.charAt(z + 3)) && 
                 (str.charAt(z + 1) == str.charAt(z + 2)) && 
                 (str.charAt(z) != str.charAt(z + 1)) &&
                 valid == true
-                ) {
+                ) { System.out.println(str.substring(z, z+4));
                     count += 1;
                     System.out.println(str);
                     z+= str.length()-z;
