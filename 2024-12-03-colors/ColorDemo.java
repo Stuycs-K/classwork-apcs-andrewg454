958e55ea9c6220dc90e889fb8654b73667353656
Andrew Glick <andrewg454@nycstudents.net>
2024-12-04 01:51:31 +0000

    done

--- 2024-12-03-colors/ColorDemo.java
+++ 2024-12-03-colors/ColorDemo.java
@@ -1,28 +1,49 @@
 public class ColorDemo{
-  public static final String CLEAR_SCREEN =  "\u001b[2J";
-public static final String HIDE_CURSOR =  "\u001b[?25l";
-public static final String SHOW_CURSOR =  "\u001b[?25h";
-  public static void main(String[] args){
-for (int i = 0; i < 56; i++){
-  go(i,i);
-  color(32, 33);
-  System.out.print(i);}
-  for (int i = 56; i >0; i--){
-    go(i,i);
-    color(32, 33);
-    System.out.print(i);}
-}
-public static int BLACK = 30;
-public static int RED = 31;
+    public static final String CLEAR_SCREEN = "\u001b[2J";
+    public static final String HIDE_CURSOR = "\u001b[?25l";
+    public static final String SHOW_CURSOR = "\u001b[?25h";
+    public static final String RESET = "\u001b[0m";
+    
+    public static final int RED = 31;
+    public static final int GREEN = 32;
+    public static final int BLUE = 34;
+    public static final int MAGENTA = 35;
+    public static final int CYAN = 36;
+    public static final int YELLOW = 33;
 
-public static void color(int foreground,int background){
-System.out.print( "\u001b[" + foreground + ";" + (10+background) + "m");
-}
+    public static final int BOLD = 1;
+    public static final int UNDERLINE = 4;
 
-public static void color(int foreground, int background, int modifier){
-System.out.print( "\u001b[" + foreground + ";" + (10+background) + ";" + modifier + "m");
-}
-public static void go(int r,int c){
-  System.out.print("\u001b[" + r + ";" + c + "f");
-}
+    public static void main(String[] args) throws InterruptedException {
+
+        System.out.print(CLEAR_SCREEN);
+        System.out.print(HIDE_CURSOR);
+
+        for (int i = 0; i < 5; i++) {
+            System.out.print(getColorAndModifier(RED, BOLD));
+            System.out.print("W");
+            System.out.print(getColorAndModifier(GREEN, UNDERLINE));
+            System.out.print("e");
+            System.out.print(getColorAndModifier(BLUE, BOLD));
+            System.out.print("l");
+            System.out.print(getColorAndModifier(MAGENTA, UNDERLINE));
+            System.out.print("c");
+            System.out.print(getColorAndModifier(CYAN, BOLD));
+            System.out.print("o");
+            System.out.print(getColorAndModifier(YELLOW, UNDERLINE));
+            System.out.print("m");
+            System.out.print(getColorAndModifier(RED, BOLD));
+            System.out.print("e");
+            System.out.print(RESET);
+
+            System.out.println();
+            Thread.sleep(500);
+        }
+
+        System.out.print(SHOW_CURSOR);
+        System.out.print(RESET);
+    }
+    public static String getColorAndModifier(int color, int modifier) {
+        return "\u001b[" + modifier + ";" + color + "m";
+    }
 }
