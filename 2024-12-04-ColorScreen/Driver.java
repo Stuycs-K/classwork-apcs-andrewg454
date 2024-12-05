44cdb4730545d1532eaea9b0a7074ace38c0ddd8
Andrew Glick <andrewg454@nycstudents.net>
2024-12-05 02:25:41 +0000

    working here not on terminal

--- 2024-12-04-ColorScreen/Driver.java
+++ 2024-12-04-ColorScreen/Driver.java
@@ -1,11 +1,13 @@
-public class Driver{
-        public static int width = 80;
-       public static int height = 30;
-    public static void main (String[] args){
-step5();
-step67();
-    }   
-    public static void step5(){
+public class Driver {
+    public static int width = 80;
+    public static int height = 30;
+
+    public static void main(String[] args) {
+        step5();
+        step67();
+    }
+
+    public static void step5() {
         int borderColor = Text.MAGENTA;
         int middle = Text.WHITE;
         Text.color(borderColor);
@@ -28,35 +30,43 @@ step67();
         }
         System.out.println();
     }
-    public static void step67(){
-        
+
+    public static void step67() {
         int[] numbers = new int[3];
         for (int i = 0; i < 3; i++) {
             double rand = Math.random();
             numbers[i] = (int)(rand * 100);
         }
+
         int spacing = 26;
         Text.go(2, 10);
+
         for (int i = 0; i < 3; i++) {
             if (numbers[i] < 25) {
                 Text.color(Text.BRIGHT, Text.RED);
             } else if (numbers[i] > 75) {
                 Text.color(Text.BRIGHT, Text.GREEN);
+            } else {
+                Text.color(Text.WHITE, Text.BLACK);
+            }
+
             System.out.print(numbers[i]);
+
             if (i < 2) {
                 for (int j = 0; j < spacing; j++) {
                     System.out.print(" ");
                 }
             }
         }
+
         System.out.println();
-        System.out.print(Text.RESET);
-    }
+        Text.color(Text.MAGENTA); 
         for (int i = 0; i < width; i++) {
-            System.out.print("-");  
+            System.out.print("-");
         }
         System.out.println();
-        
-        Text.go(31, 0); 
+
+        Text.go(31, 0);
         System.out.print(Text.RESET);
-    } }
+    }
+}
