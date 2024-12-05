a036efb1041abcf1b9bab196b1a0bcb5c459bcbe
Andrew Glick <andrewg454@nycstudents.net>
2024-12-05 00:32:18 +0000

    called func

--- 2024-12-04-ColorScreen/Driver.java
+++ 2024-12-04-ColorScreen/Driver.java
@@ -7,13 +7,13 @@ step5();
         int height = 30;
         int borderColor = Text.MAGENTA;
         int middle = Text.WHITE;
-        Text.color(borderColor, Text.background(middle));
+        Text.color(borderColor);
         for (int i = 0; i < width; i++) {
             System.out.print(" ");
         }
         System.out.println();
         for (int i = 0; i < height - 2; i++) {
-            Text.color(borderColor, Text.background(middle));
+            Text.color(middle);
             System.out.print(" ");
             for (int j = 0; j < width - 2; j++) {
                 System.out.print(" ");
@@ -21,7 +21,7 @@ step5();
             System.out.print(" ");
             System.out.println();
         }
-        Text.color(borderColor, Text.background(middle));
+        Text.color(borderColor);
         for (int i = 0; i < width; i++) {
             System.out.print(" ");
         }
