923fdd0f39cff415c9816913482c3c7f9c48be40
Andrew Glick <andrewg454@nycstudents.net>
2024-12-09 02:30:37 +0000

    testing

--- 2024-12-06-Adventurer/Driver.java
+++ 2024-12-06-Adventurer/Driver.java
@@ -1,5 +1,8 @@
 public class Driver{
   public static void main(String[] args){
     Adventurer player1 = new Wizard("Steven", 100);
+    Adventurer player2 = new Wizard("George", 100);
+    System.out.print(player1.attack(player2));
+
   }
 }
