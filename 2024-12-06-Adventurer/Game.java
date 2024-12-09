90e278ab5371c39056fefd31fd2dd7e94efc9d95
Andrew <andrewg454@nycstudents.net>
2024-12-09 10:10:54 -0500

    working

--- 2024-12-06-Adventurer/Game.java
+++ 2024-12-06-Adventurer/Game.java
@@ -1,5 +1,22 @@
+import java.util.Scanner;
 public class Game{
   public static void main(String[] args){
+    CodeWarrior Andy = new CodeWarrior("Andy", 100, "Java");
+    Wizard Enemy = new Wizard("Bob", 100);
+    System.out.println(runLoop(Andy, Enemy));
+  }
+  public static String runLoop(CodeWarrior you, Wizard enemy){
+    if(you.getHP() > 0){
+      if (enemy.getHP() > 0){
+        return "you have enough hp to begin";
+        Scanner userInput = new Scanner(System.in);
+        System.out.println("Type: (a)ttack / (sp)ecial / (su)pport / quit");
+        String response = userInput.nextLine();
+        System.out.println("response is: " + response);
 
+      }
+      return "you have defeated the Wizard";
+    }
+    return "you don't have enough HP to begin";
   }
 }
