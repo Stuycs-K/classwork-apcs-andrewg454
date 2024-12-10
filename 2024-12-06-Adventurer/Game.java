fba6100c392eca6958f92347decd394290ab61c3
Andrew Glick <andrewg454@nycstudents.net>
2024-12-10 02:27:54 +0000

    working

--- 2024-12-06-Adventurer/Game.java
+++ 2024-12-06-Adventurer/Game.java
@@ -1,22 +1,48 @@
 import java.util.Scanner;
 public class Game{
   public static void main(String[] args){
-    CodeWarrior Andy = new CodeWarrior("Andy", 100, "Java");
-    Wizard Enemy = new Wizard("Bob", 100);
+    CodeWarrior Andy = new CodeWarrior("Andy", 1, "Java");
+    Wizard Enemy = new Wizard("Bob", 20);
     System.out.println(runLoop(Andy, Enemy));
   }
+  public static String enemyTurn(CodeWarrior you, Wizard enemy){
+    int rand = (int)(Math.random() * 3);
+    System.out.println(rand);
+    if(rand == 0){
+      return enemy.attack(you);
+    }
+    if(rand == 1){
+      return enemy.specialAttack(you);
+    }
+    if(rand == 2){
+      return enemy.support(enemy);
+    }
+    return null;
+  }
   public static String runLoop(CodeWarrior you, Wizard enemy){
-    if(you.getHP() > 0){
+    System.out.println("you are a CodeWarrior starting with " + you.getHP() + " HP and your special ability is called " + you.getSpecialName());
+    while(you.getHP() > 0){
       if (enemy.getHP() > 0){
-        System.out.println("you have enough hp to begin, you are a CodeWarrior starting with " + you.getHP() + " HP and your special ability is called " + you.getSpecialName());
         Scanner userInput = new Scanner(System.in);
         System.out.println("Type: (a)ttack / (sp)ecial / (su)pport / quit");
         String response = userInput.nextLine();
         System.out.println("response is: " + response);
-
+        if (response.equals("quit")){
+          return "You Quit";
+        }
+        if (response.equals("a")){
+          System.out.println(you.attack(enemy));
+        }
+        if (response.equals("sp")){
+          System.out.println(you.specialAttack(enemy));
+        }
+        if (response.equals("su")){
+          System.out.println(you.support(you));
+        }        
+        System.out.println(enemyTurn(you, enemy));
       }
-      return "you have defeated the Wizard";
+      else{ return "you have defeated the Wizard";}
     }
-    return "you don't have enough HP to begin";
-  }
-}
+    return "you don't have enough HP";
+  }}
+
--- 2024-12-06-Adventurer/Wizard.java
+++ 2024-12-06-Adventurer/Wizard.java
@@ -1,7 +1,7 @@
 public class Wizard extends Adventurer{
     private static String specialName = "fireball";
-    private static int specialDamage = 0;
-    private static int specialResource = 1;
+    private static int specialDamage = 5;
+    private static int specialResource = 2;
     public static void main(String[] args){
 
     }
@@ -36,19 +36,20 @@ public class Wizard extends Adventurer{
   //hurt or hinder the target adventurer
   public String attack(Adventurer other){
     other.setHP(other.getHP() - 1);
-    return ("you attacked " + other + "for 1 ");
+    return ("enemy attacked " + other + "for 1 ");
   }
 
   //heall or buff the target adventurer
   public String support(Adventurer other){
     other.setHP(other.getHP() + 1);
-    return ("you healed " + other + "for 1 ");
+    other.setHP(other.getHP() + 1);
+    return ("enemy healed " + other + "for 1 ");
   }
 
   //heall or buff self
   public String support(){
     this.setHP(this.getHP() + 1);
-    return ("you supported yourself for 1 ");
+    return ("enemy supported yourself for 1 ");
   }
 
   //hurt or hinder the target adventurer, consume some special resource
@@ -57,7 +58,7 @@ public class Wizard extends Adventurer{
     other.setHP(other.getHP() - specialDamage);
     specialResource = specialResource-1;
     }
-    return ("you attacked " + other + "for " + specialDamage + "with special attack");
+    return ("enemy attacked " + other + "for " + specialDamage + "with special attack");
 
   }
 }
