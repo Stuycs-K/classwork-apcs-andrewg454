c9183904c23e564847494edce04a83a43fb11877
Andrew <andrewg454@nycstudents.net>
2025-01-02 10:01:40 -0500

    done

---
+++ 2025-01-02-fibbonaci/fib.java
@@ -0,0 +1,13 @@
+public class fib{
+  public static void main(String[] args){
+    System.out.println(fibo(1000));
+  }
+  public static int fibo(int n){
+    if(n>1){
+      return fibo(n-1) + fibo(n-2);
+    }
+    else{
+      return n;
+    }
+  }
+  }
