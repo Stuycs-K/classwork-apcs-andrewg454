e41e5bedc8d05996e11ba35d8372edc3b2f2c8ba
Andrew <andrewg454@nycstudents.net>
2025-01-03 10:15:16 -0500

    end of class

---
+++ 2025-01-02-fibbonaci/makewrds.java
@@ -0,0 +1,13 @@
+public class makewrds{
+    public static void main(String[] args){
+makeWords(3, "", "abc");
+    }
+    public static void makeWords(int remainingLetters, String result, String alphabet){
+      if(remainingLetters == 0){
+        System.out.println(result);
+      }
+      else{
+      for(int i =0; i < alphabet.length(); i++){
+        makeWords(remainingLetters -1, result + alphabet.substring(i, i+1), alphabet;)
+      }}}
+    }
